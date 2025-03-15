e826e62db9a06adca1329d15b06a829995b56f8b
usamashabbir813 <usamashabbir128@gmail.com>
2025-03-15 07:13:44 +0500

    token

--- lib/controllers/google-signin-conroller.dart
+++ lib/controllers/google-signin-conroller.dart
@@ -1,63 +1,69 @@
-// ignore_for_file: file_names, unused_field, unused_local_variable, avoid_print
+// ignore_for_file: file_names, unused_local_variable, unused_field, avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:youcollection/models/user-model.dart';
-import 'package:youcollection/user-panel/main_screen.dart';
 
-class GoogleSigninConroller extends GetxController {
-  final GoogleSignIn googleSignin = GoogleSignIn();
+import '../models/user-model.dart';
+import '../user-panel/main_screen.dart';
+import 'get-device-token-controller.dart';
+
+class GoogleSignInController extends GetxController {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> signInWithGoogle() async {
+    final GetDeviceTokenController getDeviceTokenController =
+        Get.put(GetDeviceTokenController());
     try {
       final GoogleSignInAccount? googleSignInAccount =
-          await googleSignin.signIn();
+          await googleSignIn.signIn();
+
       if (googleSignInAccount != null) {
-        EasyLoading.show(
-          status: "Please wait...",
-        );
-        GoogleSignInAuthentication googleSignInAuthentication =
+        EasyLoading.show(status: "Please wait..");
+        final GoogleSignInAuthentication googleSignInAuthentication =
             await googleSignInAccount.authentication;
 
         final AuthCredential credential = GoogleAuthProvider.credential(
-            accessToken: googleSignInAuthentication.accessToken,
-            idToken: googleSignInAuthentication.idToken);
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
 
         final UserCredential userCredential =
             await _auth.signInWithCredential(credential);
+
         final User? user = userCredential.user;
+
         if (user != null) {
           UserModel userModel = UserModel(
-              uId: user.uid,
-              username: user.displayName.toString(),
-              email: user.email.toString(),
-              phone: user.phoneNumber.toString(),
-              userImg: user.photoURL.toString(),
-              userDeviceToken: '',
-              country: '',
-              userAddress: '',
-              street: '',
-              isAdmin: false,
-              isActive: true,
-              createdOn: DateTime.now(),
-              city: '');
-          FirebaseFirestore.instance
+            uId: user.uid,
+            username: user.displayName.toString(),
+            email: user.email.toString(),
+            phone: user.phoneNumber.toString(),
+            userImg: user.photoURL.toString(),
+            userDeviceToken: getDeviceTokenController.deviceToken.toString(),
+            country: '',
+            userAddress: '',
+            street: '',
+            isAdmin: false,
+            isActive: true,
+            createdOn: DateTime.now(),
+            city: '',
+          );
+
+          await FirebaseFirestore.instance
               .collection('users')
               .doc(user.uid)
               .set(userModel.toMap());
           EasyLoading.dismiss();
-          Get.offAll(() => MainScreen());
+          Get.offAll(() => const MainScreen());
         }
       }
     } catch (e) {
       EasyLoading.dismiss();
-      print('Error $e');
+      print("error $e");
     }
   }
 }
-
-class GoogleSignin {}
